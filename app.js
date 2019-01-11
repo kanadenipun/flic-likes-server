64cde4552df386b64896b61606b766ab4a0bd803
Nipun Kanade <nipunk@thoughtworks.com>
2019-01-11 16:18:25 +0530

    Nipun: Added bluebird, nodemon, db, mongoose

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var bluebird = require('bluebird')
+var mongoose = require('mongoose')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -37,5 +40,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose.connect(process.env.TODO_DB_URL, { useMongoClient: true})
+.then(()=> { console.log(`Succesfully Connected to the Mongodb Database)})
+.catch(()=> { console.log(`Error Connecting to the Mongodb Database)})
 
 module.exports = app;
